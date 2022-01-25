290b9b75b38d234587af8beeb34559951ea6d22c
Ariyan Hridoy <87638530+ariyanhridoy777@users.noreply.github.com>
2022-01-25 14:30:36 +0600

    first c program file

---
+++ Tuhin.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{   printf("\n\n\n");
+    printf("|||||||||||||||||| \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       |_|        \n");
+    
+
+    printf("\n\n\n");
+
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("|_||||||||||||_|\n");
+    
+
+    printf("\n\n\n");
+
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |||||||||||| |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("| |          | |\n");
+    printf("|_|          |_|\n");
+    
+    printf("\n\n\n");
+
+    printf("    |||||||||       \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("       | |        \n");
+    printf("    |||||||||       \n");
+
+    printf("\n\n\n");
+
+    printf("| |||            | |\n");
+    printf("| |  ||          | | \n");
+    printf("| |    ||        | |\n");
+    printf("| |      ||      | |\n");
+    printf("| |        ||    | |\n");
+    printf("| |          ||  | | \n");
+    printf("| |            ||| |\n");
+    printf("|_|             ||_| \n");
+
+    printf("\n\n\n");
+    return 0;
+}
